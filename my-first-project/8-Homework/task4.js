7962641529c3705d00189b5954a5b67deb7ab18b
Liubov <liubov.dudnyk@gmail.com>
2025-12-09 19:29:38 +0200

    Homework of 8Lesson

---
+++ my-first-project/8-Homework/task4.js
@@ -0,0 +1,3 @@
+const pairedNumbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+const evenNumbers = pairedNumbers.filter((number) => number % 2 === 0);
+console.log(evenNumbers);
---
+++ my-first-project/8-Homework/task5.js
@@ -0,0 +1,5 @@
+const firstArray = [2,3,4,5];
+const secondArray = [6,7,8,9];
+const concatArray = firstArray.concat(secondArray);
+console.log(concatArray); 
+
